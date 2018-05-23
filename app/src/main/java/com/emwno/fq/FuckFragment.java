1405e6631a7f5f00b5c1344002ee3f514576b248
emwno <pulse.aashir@gmail.com>
2018-05-23 05:47:16 +0500

    added fffff to layout (yay!)

---
+++ app/src/main/java/com/emwno/fq/FuckFragment.java
@@ -0,0 +1,20 @@
+package com.emwno.fq;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created on 22 May 2018.
+ */
+public class FuckFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_fuck, container, false);
+        return rootView;
+    }
+
+}
