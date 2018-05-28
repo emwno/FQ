d1924633c9b1a439dfd28f6577384444173477b5
emwno <pulse.aashir@gmail.com>
2018-05-29 00:23:36 +0500

    making pojo's parcelable for communicating b/w fragment and activity

--- app/src/main/java/com/emwno/fq/network/Field.java
+++ app/src/main/java/com/emwno/fq/network/Field.java
@@ -1,13 +1,34 @@
 package com.emwno.fq.network;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created on 25 May 2018.
  */
-public class Field {
+public class Field implements Parcelable {
+
+    public static final Creator<Field> CREATOR = new Creator<Field>() {
+        @Override
+        public Field createFromParcel(Parcel in) {
+            return new Field(in);
+        }
+
+        @Override
+        public Field[] newArray(int size) {
+            return new Field[size];
+        }
+    };
 
     private String name;
     private String field;
     private String data;
+
+    protected Field(Parcel in) {
+        name = in.readString();
+        field = in.readString();
+        data = in.readString();
+    }
 
     public String getName() {
         return name;
@@ -31,5 +52,17 @@ public class Field {
 
     public void setData(String data) {
         this.data = data;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(field);
+        dest.writeString(data);
     }
 }
--- app/src/main/java/com/emwno/fq/network/Fuck.java
+++ app/src/main/java/com/emwno/fq/network/Fuck.java
@@ -1,15 +1,36 @@
 package com.emwno.fq.network;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created on 25 May 2018.
  */
-public class Fuck {
+public class Fuck implements Parcelable {
+
+    public static final Creator<Fuck> CREATOR = new Creator<Fuck>() {
+        @Override
+        public Fuck createFromParcel(Parcel in) {
+            return new Fuck(in);
+        }
+
+        @Override
+        public Fuck[] newArray(int size) {
+            return new Fuck[size];
+        }
+    };
 
     private String name;
     private String url;
     private List<Field> fields;
+
+    protected Fuck(Parcel in) {
+        name = in.readString();
+        url = in.readString();
+        fields = in.createTypedArrayList(Field.CREATOR);
+    }
 
     public String getName() {
         return name;
@@ -35,4 +56,15 @@ public class Fuck {
         this.fields = fields;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(url);
+        dest.writeTypedList(fields);
+    }
 }
