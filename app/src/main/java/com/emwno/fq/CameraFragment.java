0a10432ad03933c3d943d6e1d95be9b0c33fdaed
emwno <pulse.aashir@gmail.com>
2018-06-01 17:25:37 +0500

    add double-tap to switch camera and bu

--- app/src/main/java/com/emwno/fq/CameraFragment.java
+++ app/src/main/java/com/emwno/fq/CameraFragment.java
@@ -1,13 +1,17 @@
 package com.emwno.fq;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.configuration.CameraConfiguration;
 import io.fotoapparat.selector.LensPositionSelectorsKt;
 import io.fotoapparat.selector.ResolutionSelectorsKt;
 import io.fotoapparat.view.CameraView;
@@ -18,22 +22,35 @@ import io.fotoapparat.view.CameraView;
 public class CameraFragment extends Fragment {
 
     private Fotoapparat mCamera;
+    private int mLensPosition = 0;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setUserVisibleHint(false);
-    }
+    GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
 
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            if (mLensPosition == 0) {
+                mCamera.switchTo(LensPositionSelectorsKt.front(), CameraConfiguration.standard());
+                mLensPosition++;
+            } else {
+                mCamera.switchTo(LensPositionSelectorsKt.back(), CameraConfiguration.standard());
+                mLensPosition--;
+            }
+            return true;
+        }
+    });
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_camera, container, false);
 
         CameraView cameraView = rootView.findViewById(R.id.camera_view);
         mCamera = Fotoapparat.with(getContext())
-                .lensPosition(LensPositionSelectorsKt.front())
+                .lensPosition(LensPositionSelectorsKt.back())
                 .photoResolution(ResolutionSelectorsKt.highestResolution())
                 .into(cameraView).build();
+
+        cameraView.setOnTouchListener((v, event) -> !gestureDetector.onTouchEvent(event));
 
         return rootView;
     }
