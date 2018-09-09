72165c3bf0543fbe011ecfb0c7fd98083c7329db
emwno <pulse.aashir@gmail.com>
2018-09-09 01:51:02 -0400

    bugfix of bugfix: should compare absolute distance

--- app/src/main/java/com/emwno/fq/ui/QuoteFragment.java
+++ app/src/main/java/com/emwno/fq/ui/QuoteFragment.java
@@ -47,12 +47,14 @@ public class QuoteFragment extends Fragment implements GestureListener {
             mTouchStartPointY = e1.getRawY();
         }
 
-        if (distanceY > distanceX && mTouchStartPointY - e2.getRawY() > 100) {
-            mTouchStartPointY = e2.getRawY();
-            mListener.onAdjustFQSize(0, Math.abs(distanceY) / 100);
-        } else if (distanceY > distanceX && e2.getRawY() - mTouchStartPointY > 100) {
-            mTouchStartPointY = e2.getRawY();
-            mListener.onAdjustFQSize(1, Math.abs(distanceY) / 100);
+        if (Math.abs(distanceY) > Math.abs(distanceX)) {
+            if (mTouchStartPointY - e2.getRawY() > 100) {
+                mTouchStartPointY = e2.getRawY();
+                mListener.onAdjustFQSize(0, Math.abs(distanceY) / 100);
+            } else if (e2.getRawY() - mTouchStartPointY > 100) {
+                mTouchStartPointY = e2.getRawY();
+                mListener.onAdjustFQSize(1, Math.abs(distanceY) / 100);
+            }
         }
 
         return true;
